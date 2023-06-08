b7ac4faa1192aa3f9c3b47330aeb573b772b1143
Jimmy@devs <ssonkojimmy@gmail.com>
2023-06-08 23:18:58 +0300

    (Rest api routing): Create an endpoint for creating users
    - Create an end point for the create user service

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
-  port: 1337,
+  port: 8080,
   dbUri:
     "mongodb+srv://MubiruIsaac:YJ9VPGQFRy2qEb5i@renewableenergy.tur0voc.mongodb.net/renewableEnergy?retryWrites=true&w=majority",
   saltWorkFactor: 10,
